0c466da21d865c721933af089bbc8bf202ad3d09
krez <kmfreas@gmail.com>
2018-04-14 12:19:21 -0400

    update composer-update-all

--- src/composer-update-all.sh
+++ src/composer-update-all.sh
@@ -1,34 +1,33 @@
 #!/bin/bash
-
-BLUE="\033[0;34m"
-NOCOLOR='\033[0m'
-
-
 cd ../sites
 DIR=$PWD
+WPVERSION='4.9.5'
+GREEN="\033[0;32m"
+NOCOLOR='\033[0m'
 
-echo $DIR
+SITES=`ls | tr "\n" " "`
+
 for DIRECTORY in `ls`
 do
-    printf "${BLUE}\n\n================ \n $DIRECTORY \n================ \n${NOCOLOR}"
-    cd $DIRECTORY
-    git status --porcelain
-    git stash
-    git checkout production
-    git pull
-    cd ../../trellis/
+    printf "\n\n================ \n $DIRECTORY \n================ \n"
     echo "Skip?"
     read INPUT
     if [ "y" != $INPUT ]; then
+        cd $DIRECTORY
+        git status --porcelain
+        git stash
+        git checkout production
+        git pull
 
+        cd ../../trellis/
         vagrant ssh -- -t "composer remove johnpbloch/wordpress -d /srv/www/$DIRECTORY/current/"
         vagrant ssh -- -t "composer clear-cache -d /srv/www/$DIRECTORY/current/"
-        vagrant ssh -- -t "composer require johnpbloch/wordpress:4.8.3 -d /srv/www/$DIRECTORY/current/"
+        vagrant ssh -- -t "composer require johnpbloch/wordpress:$WPVERSION -d /srv/www/$DIRECTORY/current/"
         vagrant ssh -- -t "composer update -d /srv/www/$DIRECTORY/current/"
         cd $DIR
         cd $DIRECTORY
         git add -u
-        git commit -m 'update wordpress to 4.8.3, update plugins'
+        git commit -m "update wordpress to $WPVERSION, update plugins"
         git push
         cd ../../trellis/
         ./bin/deploy.sh production $DIRECTORY
@@ -36,3 +35,6 @@ do
 
     cd $DIR
 done
+
+printf "${GREEN}\n\nUpdate complete. You should check all sites to ensure the update did not break anything. Run the below string to open all sites in Chrome\n\n"
+printf "/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome $SITES${NOCOLOR}\n\n"
